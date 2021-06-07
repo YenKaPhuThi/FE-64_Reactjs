b68fdc5b3fc249581fbfc8365e81581f9be7b296
Yen Chipu <yenkaphu96pnv@gmail.com>
2021-06-07 07:05:05 +0700

    [React]: Praticse with binding data again

--- src/components/BindingData/Student.jsx
+++ src/components/BindingData/Student.jsx
@@ -1,24 +1,34 @@
 import React, { Component } from "react";
 
 export default class Student extends Component {
-  hotTen = "Yen";
-  class = "FE 64";
+  // Properties
+  firstName = "Yen";
+  lastName = "Ka Phu";
+  middleName = "Thi"
 
-  arrangeLevel = (type) => {
-    return (
-      <div>
-        <p>{type}</p>
-        <p>{this.hotTen}</p>
-        <p>{this.class}</p>
-      </div>
-    );
-  };
+  // Method
+  arrangeStudentLevel (point) {
+    let result = '';
+    if (8 < point <= 10) {
+      result = "Excellence"
+    } else if (6 < point <= 8) {
+      result = "Good"
+    } else if (0 <= point <= 6) {
+      result = "Bad"
+    } else {
+      result = "Not valid point"
+    }
+    return result;
+  }
+
 
   render() {
     let point = 10;
     return (
       <div>
-        Student: {point} {this.arrangeLevel("Giỏi")}
+        <h3>Student information</h3>
+        <p>Full name: {this.firstName + " " + this.lastName + " " + this.middleName}</p>
+        <p>Level: {this.arrangeStudentLevel(point)}</p>
       </div>
     );
   }
